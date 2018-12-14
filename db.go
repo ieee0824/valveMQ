4372b8a9109d8c6b953abc2a7b8c15395345fce7
ieee0824 <ieee0824x@gmail.com>
2018-12-14 14:18:20 +0900

    fix default value

--- db.go
+++ db.go
@@ -21,10 +21,10 @@ func DBInit(cfg *Config) error {
 	}
 	tx := d.MustBegin()
 
-	if _, err := tx.Exec("CREATE DATABASE IF NOT EXISTS `mq` DEFAULT CHARACTER SET utf8mb4"); err != nil {
+	if _, err := tx.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s DEFAULT CHARACTER SET utf8mb4", cfg.DbName)); err != nil {
 		return err
 	}
-	if _, err := tx.Exec("use mq"); err != nil {
+	if _, err := tx.Exec(fmt.Sprintf("use %s", cfg.DbName)); err != nil {
 		return err
 	}
 	if _, err := tx.Exec("CREATE TABLE IF NOT EXISTS `message` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`body` text NOT NULL,`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,`expire` timestamp NULL DEFAULT NULL,`flag` int(11) NOT NULL DEFAULT '0',`hash` varchar(512) NOT NULL DEFAULT '',PRIMARY KEY (`id`),KEY `hash` (`hash`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;"); err != nil {
@@ -33,6 +33,17 @@ func DBInit(cfg *Config) error {
 
 	if _, err := tx.Exec("CREATE TABLE IF NOT EXISTS `log` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `last_dequeue_time` timestamp(6) NOT NULL DEFAULT '1980-01-01 00:00:00.000000', `hash` varchar(512) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;"); err != nil {
 		return err
+	}
+
+	var n int
+	if err := tx.Get(&n, "SELECT COUNT(id) FROM log WHERE id = 1"); err != nil {
+		return err
+	}
+	if n == 0 {
+		_, err := tx.Exec("INSERT INTO log (id, hash) VALUES (1, \"\")")
+		if err != nil {
+			return err
+		}
 	}
 
 	tx.Commit()
